3ed9a92054c2f8124c07231e06d43fb78ba2ece5
Tina RAJAONARIVELO <andriarilala.rajaonarivelo@supdevinci-edu.fr>
2025-05-13 17:45:50 +0200

    add spacy library to requirements.txt + delete test.py and .vscode

--- test.py
+++
@@ -1,9 +0,0 @@
-from db import get_engine
-
-engine = get_engine()
-
-try:
-    with engine.connect() as connection:
-        print("Connexion réussie à la base de données !")
-except Exception as e:
-    print(f"Erreur de connexion ou de création de tables: {e}")
